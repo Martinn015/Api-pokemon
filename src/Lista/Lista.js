0ae031424593a6831ce7a18a8457f570e6d36f2b
Martinn015 <103222420+Martinn015@users.noreply.github.com>
2022-07-22 00:15:15 -0300

    listaok

--- src/Lista/Lista.js
+++ src/Lista/Lista.js
@@ -1,9 +1,38 @@
-const Lista = (props) => {
+import React, {useEffect, useInsertionEffect, useState} from "react"
+
+const Pokemon = (props) => {
+    const [pokemons, setPokemons] = useState([])
+    const [titulo, setTitulo] = useState("Hola soy un titulo")
+
+
+    useEffect(() => {
+        console.log("Soy el userEffect gatillandose ;0")
+    },[titulo])
+
+    useEffect( () => {
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+        .then(res => res.json())
+        .then(
+            (data) => {
+                setPokemons(data.results)
+            }
+        )
+    })
+       
+    const handleClick = () => {
+        setTitulo("Ahora soy un titulo")
+    }
+    
     return (
         <>
-            Lista Pokemon !
+            {titulo}
+            <button onClick={handleClick}> Cambiar titulo</button>
+            {pokemons.map((pokemon) => {
+                return(<div>{pokemon.name}</div>)
+            })}
+            Pokemon Funciona !  
         </>
     )
 }
 
-export default Lista
+export default Pokemon
